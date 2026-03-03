31c6702d3c306eb44acb6fd6e70d596005e141ff
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-03-03 23:11:20 +0530

    defined controller function for get current logged in user

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,3 +60,17 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+//Get Current Logged in User
+exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
